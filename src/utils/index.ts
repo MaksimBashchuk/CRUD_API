a54f6835f50ef215baa49f896f8e3df29e5225dc
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2022-06-18 02:25:55 +0300

    refactor: change error response creator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
-import { ServerResponse } from 'http';
+import { ServerResponse, STATUS_CODES } from 'http';
 
-export const sendErrorResponse = (res: ServerResponse, statusCode: number, message: string) => {
+export const sendErrorResponse = (res: ServerResponse, statusCode: number, msg: string) => {
+  const message = `${STATUS_CODES[statusCode]}: ${msg}`;
   res.writeHead(statusCode, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ message }));
 };
